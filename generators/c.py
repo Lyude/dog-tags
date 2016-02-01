ba93fdb6aee6e9c7ba7fa0c4b0bfb14842f819c5
Lyude <thatslyude@gmail.com>
2016-01-31 19:12:57 -0500

    Add file-local tags

--- generators/c.py
+++ generators/c.py
@@ -4,26 +4,54 @@ from sys import argv
 from ctag import CTag
 from syntax import KeywordHighlight
 
+keyword_highlights = list()
+
+def add_function_tags(c_tags):
+	global_tags = dict()
+	local_tags = dict()
+	keyword_highlight = KeywordHighlight("cFunctionTag", "Function")
+
+	tags = CTag.filter_by_attr(c_tags, "tag_type", "f") + \
+	       CTag.filter_by_attr(c_tags, "tag_type", "p")
+
+	for tag in tags:
+		if tag.tag_type == "p" and tag.file_name.endswith(".h"):
+			global_tags[tag.tag_name] = tag
+
+			if tag.tag_name in local_tags:
+				local_tags.remove(local_tag.tag_name)
+		else:
+			if tag.tag_name in local_tags:
+				continue
+
+			local_tags[tag.tag_name] = tag
+
+	keyword_highlight.add_tags(global_tags)
+	for local_tag in local_tags:
+		keyword_highlight.add_tags(tag, tag.file_name)
+
+	keyword_highlights.append(keyword_highlight)
+
+def add_tags(name, highlight_group, c_tags, tag_type):
+	keyword_highlight = KeywordHighlight(name, highlight_group)
+	tags = CTag.filter_by_attr(c_tags, "tag_type", tag_type)
+
+	for tag in tags:
+		if tag.file_name.endswith(".h"):
+			keyword_highlight.add_tags(tag)
+		else:
+			keyword_highlight.add_tags(tag, tag.file_name)
+
+	keyword_highlights.append(keyword_highlight)
+
 def generate_syntax(tags_list):
 	c_tags = CTag.filter_by_attr(tags_list, "language", "C++") + \
 		 CTag.filter_by_attr(tags_list, "language", "C")
-	keyword_highlights = list()
 
-	current_rule = KeywordHighlight("cFunctionTag", "Function")
-	current_rule.add_tags(CTag.filter_by_attr(c_tags, "tag_type", "f"))
-	keyword_highlights.append(current_rule)
-
-	current_rule = KeywordHighlight("cMacroTag", "Macro")
-	current_rule.add_tags(CTag.filter_by_attr(c_tags, "tag_type", "d"))
-	keyword_highlights.append(current_rule)
-
-	current_rule = KeywordHighlight("cEnumTag", "Constant")
-	current_rule.add_tags(CTag.filter_by_attr(c_tags, "tag_type", "e"))
-	keyword_highlights.append(current_rule)
-
-	current_rule = KeywordHighlight("cTypeTag", "Type")
-	current_rule.add_tags(CTag.filter_by_attr(c_tags, "tag_type", "t"))
-	keyword_highlights.append(current_rule)
+	add_function_tags(c_tags)
+	add_tags("cMacroTag", "Macro", c_tags, "d")
+	add_tags("cEnumTag", "Constant", c_tags, "e")
+	add_tags("cTypeTag", "Type", c_tags, "t")
 
 	return keyword_highlights
 
--- syntax.py
+++ syntax.py
@@ -3,27 +3,54 @@
 class KeywordHighlight():
 	def __init__(self, name, highlight_group):
 		self.global_tags = list()
+		self.local_tags = dict()
 
 		self.name = name
 		self.highlight_group = highlight_group
 
-	def add_tags(self, tags):
+	def add_tags(self, tags, scope=None):
 		if hasattr(tags, "__iter__"):
 			for tag in tags:
-				self.add_tags(tag)
+				self.add_tags(tag, scope)
 
 			return
 
+		if scope != None:
+			if scope not in self.local_tags:
+				self.local_tags[scope] = list()
 
-		self.global_tags.append(tags)
+			dest = self.local_tags[scope]
+		else:
+			dest = self.global_tags
+
+		dest.append(tags)
 
 	def __str__(self):
-		ret = "syn keyword %s " % self.name
+		ret = ""
 
-		for keyword in self.global_tags:
-			ret += keyword.tag_name + " "
+		if len(self.global_tags) != 0:
+			ret += "syn keyword %s " % self.name
 
-		ret += "\n"
-		ret += "hi def link %s %s\n" % (self.name, self.highlight_group)
+			for tag in self.global_tags:
+				ret += tag.tag_name + " "
+
+			ret += "\n"
+
+		for scope in self.local_tags.keys():
+			ret += "if expand('%%:t') == '%s'\n" % scope
+			ret += "\tsyn keyword %s " % self.name
+
+			for tag in self.local_tags[scope]:
+				ret += tag.tag_name + " "
+
+			ret += "\n"
+
+			if len(self.global_tags) == 0:
+				ret += "\thi def link %s %s\n" % (self.name, self.highlight_group)
+
+			ret += "endif\n"
+
+		if len(self.global_tags) != 0:
+			ret += "hi def link %s %s\n" % (self.name, self.highlight_group)
 
 		return ret
