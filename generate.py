b709586bd1cc8ff2fec1465103f894a00b882272
Lyude <thatslyude@gmail.com>
2016-11-11 14:50:10 -0500

    Add progress indicator

--- generate.py
+++ generate.py
@@ -4,27 +4,62 @@ import importlib
 import argparse
 from fnmatch import fnmatch
 from os import path
+from sys import stderr
 from ctag import CTag
 from syntax import KeywordHighlight
 
+class ProgressIndicator():
+	def __init__(self, tag_count):
+		self.processed_count = 0
+		self.filtered_count = 0
+		self.tag_count = tag_count
+		self.progress_str = "0/%d tags (0%%)" % tag_count
+
+		stderr.write("Processed %s" % self.progress_str)
+		stderr.flush()
+
+	def update(self):
+		stderr.write('\b' * len(self.progress_str))
+		count = self.processed_count + self.filtered_count
+
+		self.progress_str = "%d/%d tags (%d%%)" % ( \
+				count, self.tag_count, (count / self.tag_count) * 100)
+		stderr.write(self.progress_str)
+		stderr.flush()
+
+	def finish(self):
+		stderr.write('\n')
+		stderr.write("%d included, %d excluded (filtered %d%%)\n" % ( \
+				self.processed_count, self.filtered_count,
+				(self.filtered_count / self.tag_count) * 100))
+		stderr.flush()
+
 def parse_tag_file(args):
 	tag_list = list()
+	tag_lines = open(args.tag_file).readlines()
 
-	for line in open(args.tag_file).readlines():
+	progress = ProgressIndicator(len(tag_lines))
+
+	for line in tag_lines:
 		try:
 			tag = CTag(line)
 
 			if args.include != None and \
-			   not any(fnmatch(tag.file_name, glob) for glob in args.include):
-				continue
+					not any(fnmatch(tag.file_name, glob) for glob in args.include):
+			   progress.filtered_count += 1
+			elif args.exclude != None and \
+					any(fnmatch(tag.file_name, glob) for glob in args.exclude):
+			   progress.filtered_count += 1
+			else:
+			   progress.processed_count += 1
+			   tag_list.append(tag)
 
-			if args.exclude != None and \
-			   any(fnmatch(tag.file_name, glob) for glob in args.exclude):
-				continue
+			progress.update()
 
-			tag_list.append(tag)
 		except CTag.NotTagException:
 			pass
+
+	progress.finish()
 
 	return tag_list
 
@@ -38,6 +73,7 @@ if __name__ == "__main__":
 					    metavar='pattern', action='append', dest='include')
 	args = parser.parse_args()
 
+	stderr.write("Reading tag list...\n")
 	tag_list = parse_tag_file(args)
 
 	# TODO: Eventually I'll have some proper search logic to find generators
@@ -47,6 +83,7 @@ if __name__ == "__main__":
 	sys.path.append(path.join(path.dirname(path.realpath(__file__)), "generators"))
 
 	generator = importlib.import_module(args.filetype)
+	stderr.write("Generating syntax highlighting...\n")
 	syntax = generator.generate_syntax(tag_list)
 
 	# Clear the current syntax in vim in case the script's loaded multiple
