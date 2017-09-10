f2e8b2097cc14f78738f81086272d34d8773107f
Lyude Paul <thatslyude@gmail.com>
2017-09-10 13:15:11 -0400

    output: Make naming of some variables more appropriate

--- dogtags/output.py
+++ dogtags/output.py
@@ -1,20 +1,20 @@
 class Output():
     def __init__(self):
-        self._in_new_line = False
-        self._at_start_of_line = True
+        self.__in_new_line = False
+        self.__at_start_of_line = True
         self.indent_level = 0
 
     def __call__(self, string, endline=True):
-        if self._in_new_line:
+        if self.__in_new_line:
             self._out_func('\n')
 
-        if self._at_start_of_line:
+        if self.__at_start_of_line:
             self._out_func("  " * self.indent_level)
 
         self._out_func(string)
 
-        self._in_new_line = endline
-        self._at_start_of_line = endline
+        self.__in_new_line = endline
+        self.__at_start_of_line = endline
 
 class FileOutput(Output):
     def __init__(self, file):
