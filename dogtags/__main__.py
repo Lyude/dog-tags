f93e47e7796126c9daedd37014d60dff7687c27d
Lyude Paul <thatslyude@gmail.com>
2017-09-10 14:44:52 -0400

    __main__: Improve error output from invalid arguments

--- dogtags/__main__.py
+++ dogtags/__main__.py
@@ -8,8 +8,23 @@ from dogtags.syntax import ConditionalBlock, generate_init_script
 from dogtags.version import __version__
 from sys import stderr, stdout, exit
 
+def parse_filetype(arg):
+    try:
+        return importlib.import_module('dogtags.generators.' + arg)
+    except ModuleNotFoundError:
+        raise argparse.ArgumentTypeError("'%s' generator not found" % arg)
+
+def parse_output(arg):
+    try:
+        return FileOutput(open(arg, 'w'))
+    except OSError as e:
+        raise argparse.ArgumentTypeError(
+            "Can't open '%s': %s" % (arg, e.strerror))
+
 parser = argparse.ArgumentParser(description="Generate vim syntax files using ctags")
-parser.add_argument('filetype', help="The filetype we're generating highlighting from")
+parser.add_argument('filetype',
+                    help="The filetype we're generating highlighting from",
+                    type=parse_filetype)
 parser.add_argument('tag_file', help="The ctags file to use for generation",
                     type=argparse.FileType('r'))
 parser.add_argument('-V', '--version', action='version', version=__version__)
@@ -19,15 +34,13 @@ parser.add_argument('-i', '--include', help="Include only tags from files matchi
                     metavar='pattern', action='append', dest='include')
 parser.add_argument('-o', '--output',
                     help="Where to output the generated syntax file (default is /dev/stdout)",
-                    type=lambda output: FileOutput(open(output, 'w')),
-                    default=FileOutput(stdout))
+                    type=parse_output, default=FileOutput(stdout))
 parser.add_argument('-a', '--add-library',
                     help="Include globally visible tags from another project in the output",
                     action='append', type=argparse.FileType(), dest='libraries')
 args = parser.parse_args()
 
-generator_module = importlib.import_module("dogtags.generators." +
-                                           args.filetype)
+generator_module = args.filetype
 assert hasattr(generator_module, "Generator")
 assert issubclass(generator_module.Generator, GeneratorBase)
 
